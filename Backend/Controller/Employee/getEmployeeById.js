46a06573e0c30361d05035d43762656a12279c8f
Revathi <revathibecsc@gmail.com>
2023-10-24 14:35:47 +0530

    Get employee

--- Backend/Controller/Employee/getEmployeeById.js
+++ Backend/Controller/Employee/getEmployeeById.js
@@ -4,56 +4,57 @@ module.exports = (req, res) => {
   const employeeId = req.params.id;
   console.log("Requested Id is", employeeId);
   const getEmployeeSQL = `
-    SELECT e.employee_id, 
-           e.name, 
-           e.profile_pic,
-           e.education,
-           e.experience,
-           e.designation,
-           e.isAdmin,
-           pi.dob, 
-           pi.marital_status, 
-           pi.gender, 
-           pi.place, 
-           cd.mobile_number, 
-           cd.email, 
-           ei.alternative_phone_number, 
-           ei.physically_challenged, 
-           GROUP_CONCAT(s.skill_name) AS skills,
-           ex.experience_description, 
-           p.portfolio_url, 
-           p.github_url,
-           a.password
-    FROM employees e
-    LEFT JOIN personal_information pi ON e.employee_id = pi.employee_id
-    LEFT JOIN contact_details cd ON e.employee_id = cd.employee_id
-    LEFT JOIN extra_information ei ON e.employee_id = ei.employee_id
-    LEFT JOIN employee_skills es ON e.employee_id = es.employee_id
-    LEFT JOIN skills s ON es.skill_id = s.skill_id
-    LEFT JOIN experience ex ON e.employee_id = ex.employee_id
-    LEFT JOIN projects p ON e.employee_id = p.employee_id
-    LEFT JOIN admin a ON e.employee_id = a.employee_id  
+  SELECT
+  e.employee_id,
+  e.name,
+  e.profile_pic,
+  e.education,
+  e.experience,
+  e.designation,
+  e.isAdmin,
+  pi.dob,
+  pi.marital_status,
+  pi.gender,
+  pi.place,
+  cd.mobile_number,
+  cd.email,
+  ei.alternative_phone_number,
+  ei.physically_challenged,
+  GROUP_CONCAT(es.skill_name) AS skills,
+  ex.experience_description,
+  p.portfolio_url,
+  p.github_url,
+  a.password
+FROM employees e
+LEFT JOIN personal_information pi ON e.employee_id = pi.employee_id
+LEFT JOIN contact_details cd ON e.employee_id = cd.employee_id
+LEFT JOIN extra_information ei ON e.employee_id = ei.employee_id
+LEFT JOIN employee_skills es ON e.employee_id = es.employee_id
+LEFT JOIN experience ex ON e.employee_id = ex.employee_id
+LEFT JOIN projects p ON e.employee_id = p.employee_id
+LEFT JOIN admin a ON e.employee_id = a.employee_id
+WHERE e.employee_id = ?
+GROUP BY e.employee_id,
+       e.name,
+       e.education,
+       e.profile_pic,
+       e.experience,
+       e.designation,
+       e.isAdmin,
+       pi.dob,
+       pi.marital_status,
+       pi.gender,
+       pi.place,
+       cd.mobile_number,
+       cd.email,
+       ei.alternative_phone_number,
+       ei.physically_challenged,
+       ex.experience_description,
+       p.portfolio_url,
+       p.github_url,
+       a.password;
 
-    WHERE e.employee_id = ?
-    GROUP BY e.employee_id, 
-             e.name, 
-             e.education,
-             e.profile_pic,
-             e.experience,
-             e.designation,
-             e.isAdmin,
-             pi.dob, 
-             pi.marital_status, 
-             pi.gender, 
-             pi.place, 
-             cd.mobile_number, 
-             cd.email, 
-             ei.alternative_phone_number, 
-             ei.physically_challenged, 
-             ex.experience_description, 
-             p.portfolio_url, 
-             p.github_url,
-             a.password;
+
   `;
 
   connection.query(getEmployeeSQL, [employeeId], (err, results) => {
@@ -68,7 +69,7 @@ module.exports = (req, res) => {
     // Process the "skills" field into an array
     const employee = results[0];
     if (employee.skills) {
-      employee.skills = employee.skills.split(',').map(skill => skill.trim());
+      employee.skills = employee.skills.split(",").map((skill) => skill.trim());
     }
 
     res.status(200).json({ employee });
